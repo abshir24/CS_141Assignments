0c978ad2d50faea5a1bd0f4a91fc3d91cf60e040
Abshir <abshirm@live.com>
2018-05-23 12:28:08 -0700

    initial commit

---
+++ HelloWorld (1).java
@@ -0,0 +1,16 @@
+// Abshir Mohamed  Spring 2018 CS& 141 3:00-3:50
+// Programming Assignment #1, 04/05/2018
+//
+// This programs purpose is to print out a box made of backslashes and foward slashes to the console and inside the box is
+//information about myself and the things that I like
+
+
+public class HelloWorld {
+	public static void main(String[] args)
+	{
+		System.out.println("/////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\n// Abshir Mohamed\t\t\\\\\n// Abshir\t\t\t\\\\\n// Third row first seat\t\t\\\\\n// Reggae music\t\t\t\\\\\n/////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+		//Longer way 
+//		System.out.println("/////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\n// Abshir Mohamed               \\\\\n// Abshir                       \\\\\n// Third row first seat         \\\\\n// Reggae music                 \\\\\n/////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+	}
+	
+}
