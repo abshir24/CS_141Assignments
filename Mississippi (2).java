bfd7174a9b53facc67c38a647dafe0feeefe27e7
Abshir <abshirm@live.com>
2018-05-23 12:29:02 -0700

    Assignment 2

---
+++ Mississippi (2).java
@@ -0,0 +1,72 @@
+// Abshir Mohamed
+
+// CS&141
+
+// Assignment #2
+
+// This program will print out the word Mississipi in block letters on the console.
+
+
+public class Mississippi {
+	public static void main(String[] args)
+	{
+		M();
+		I();
+		S();
+		S();
+		I();
+		S();
+		S();
+		I();
+		P();
+		P();
+		I();
+	}
+	
+	public static void M()
+	{
+		System.out.println("M         M");
+		System.out.println("MM       MM");
+		System.out.println("M   M  M  M");
+		System.out.println("M    M    M");
+		System.out.println("M         M");
+		System.out.println("M         M");
+		System.out.println("M         M");
+		System.out.println("M         M");
+	}
+	
+	public static void I()
+	{
+		System.out.println("IIIIIII");
+		System.out.println("   I   ");
+		System.out.println("   I   ");
+		System.out.println("   I   ");
+		System.out.println("   I   ");
+		System.out.println("   I   ");
+		System.out.println("IIIIIII");
+	}
+	
+	public static void S()
+	{
+		System.out.println(" SSSSSS");
+		System.out.println("S      S");
+		System.out.println("S       ");
+		System.out.println(" SSSSSS");
+		System.out.println("       S");
+		System.out.println("S      S");
+		System.out.println(" SSSSSS");
+	}
+
+	public static void P()
+	{
+		System.out.println("PPPPPPPP");
+		System.out.println("P      P");
+		System.out.println("P      P");
+		System.out.println("PPPPPPPP");
+		System.out.println("P       ");
+		System.out.println("P       ");
+		System.out.println("P       ");
+	}
+	
+}
+
