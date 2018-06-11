252a1d609f3d3441c145e2bdc3cbdebfaa072302
Abshir <abshirm@live.com>
2018-06-11 09:19:49 -0700

    assingment 11

---
+++ AssignmentEleven.java
@@ -0,0 +1,31 @@
+// Abshir Mohamed
+
+// CS&141
+
+// Assignment #11
+
+//This is a program that searches through a file filled with numbers and finds the smallest number;
+
+import java.io.*;
+import java.util.*;
+
+public class AssignmentEleven {
+	
+	public static void main(String[] arg)throws FileNotFoundException
+	{
+//		File file = new File("src/numbers.txt");
+//		
+//		Scanner console = new Scanner(file);
+//		
+//		double min = console.nextDouble();
+//		
+//		while(console.hasNextDouble())
+//		{
+//			double number = console.nextDouble();
+//			
+//			if(number < min) { min = number; }
+//		}
+//		
+//		System.out.printf("The minimum number is %d ",(int)min);
+	}
+}
